0ddeb25f47da5a0c9e7ba2a9497d8f4d6f9a0bcc
epi <epi@atari8.info>
2011-04-06 00:25:29 +0200

    [xebin] Added options: extract, disassemble. Updated documentation. Simplified build process.

--- Makefile
+++ Makefile
@@ -1,51 +1,35 @@
-VERSION = 1.0.0
-OBX = fp21depk.obx fp21depk_noint.obx
-TABLES = fp21depk.tab fp21depk_noint.tab
-AUTO_D = fp21depktab.d
-SOURCES = flashpack.d binary.d xebin.d
-
-DC = dmd -O -release -inline -of$@
-PERL = perl
-XASM = xasm $< -o $@
-XASM_TAB = $(XASM) -t $(subst .obx,.tab,$@)
+VERSION = 1.0.1
+SOURCES = flashpack.d binary.d disasm.d xasm.d xebin.d
+DMD = dmd -O -release -inline -of$@
 ASCIIDOC = asciidoc -o $@ -a doctime
-ASCIIDOC_POSTPROCESS = perl -pi.bak -e "s/527bbd;/20a0a0;/;END{unlink '$@.bak'}" $@
+ASCIIDOC_POSTPROCESS = perl -pi.bak -e "END{unlink '$@.bak'}" $@
 ASCIIDOC_VALIDATE = xmllint --valid --noout --nonet $@
 ZIP = 7z a -mx=9 -tzip $@
 RM = rm -f
 
-OS := $(shell uname -s)
-ifneq (,$(findstring windows,$(OS)))
-EXESUFFIX=.exe
+ifdef ComSpec
+EXESUFFIX = .exe
+else
+ifdef COMSPEC
+EXESUFFIX = .exe
 endif
-ifneq (,$(findstring Cygwin,$(OS)))
-EXESUFFIX=.exe
-endif
-ifneq (,$(findstring MINGW,$(OS)))
-EXESUFFIX=.exe
-endif
+endif 
+
 XEBIN_EXE=xebin$(EXESUFFIX)
 
-all: $(XEBIN_EXE) xebin.html
+all: $(XEBIN_EXE)
+
+doc: xebin.html
 
 windist: xebin-$(VERSION)-windows.zip
 
 srcdist: xebin-$(VERSION)-src.zip
 
 debug:
-	$(MAKE) DC="dmd -unittest -debug -w -wi -of$(XEBIN_EXE)"
+	$(MAKE) DMD="dmd -unittest -debug -w -wi -of$(XEBIN_EXE)" clean all
 
-$(XEBIN_EXE): $(SOURCES) $(AUTO_D) $(OBX)
-	$(DC) $(SOURCES) $(AUTO_D) -J.
-
-fp21depktab.d: tab2d.pl $(TABLES:.tab=.obx)
-	perl tab2d.pl $(TABLES) >$@
-
-fp21depk.obx fp21depk.tab: fp21depk.asx
-	$(XASM_TAB) -d NO_INT=0
-
-fp21depk_noint.obx fp21depk_noint.tab: fp21depk.asx
-	$(XASM_TAB) -d NO_INT=1
+$(XEBIN_EXE): $(SOURCES)
+	$(DMD) $(SOURCES)
 
 xebin.html: README.asciidoc
 	$(ASCIIDOC) $<
@@ -65,8 +49,10 @@ xebin-$(VERSION): $(SOURCES) tab2d.pl fp21depk.asx README.asciidoc makefile
 	( mkdir xebin-$(VERSION) && cp $^ xebin-$(VERSION)/ )
 
 clean:
-	$(RM) $(XEBIN_EXE) xebin.o $(SOURCES:.d=.obj) $(SOURCES:.d=.map) $(OBX) $(TABLES) $(AUTO_D) $(AUTO_D:.d=.obj) $(AUTO_D:.d=.map)
+	$(RM) $(XEBIN_EXE) xebin.o $(SOURCES:.d=.obj) $(SOURCES:.d=.map)
 	$(RM) xebin.html xebin-$(VERSION)-windows.zip xebin-$(VERSION)-src.zip
 	$(RM) -r xebin-$(VERSION)
+
+.PHONY: all doc debug windist srcdist clean
 	
 .DELETE_ON_ERROR:
--- xebin.d
+++ xebin.d
@@ -28,12 +28,14 @@ import std.getopt;
 
 import binary;
 import flashpack;
+import disasm;
 
 int address = 0xffff;
-//int position;
+int position;
 bool verbose;
 string outputFile;
 bool disableOs;
+bool removeHeader;
 
 immutable VERSION_STRING = "1.0.1";
 
@@ -67,8 +69,26 @@ void list(string[] args)
 	{
 		if (args.length > 3)
 			of.writeln(file.name, ":");
-		foreach (i, blk; BinaryFileReader(file).readFile())
-			of.writefln("%3d. %s", i, blk);
+		auto blocks = BinaryFileReader(file).readFile();
+		foreach (i, blk; blocks)
+		{
+			of.writef("%3d. %s", i, blk);
+			switch (detectCompressionMethod(blocks[i .. $]))
+			{
+			case CompressionMethod.FLASHPACK_10:
+				of.writeln(" (FlashPack 1.0)");
+				break;
+			case CompressionMethod.FLASHPACK_21:
+				of.writeln(" (FlashPack 2.1)");
+				break;
+			case CompressionMethod.FLASHPACK_21_OS_DISABLED:
+				of.writeln(" (FlashPack 2.1, OS ROM disabled)");
+				break;
+			default:
+				of.writeln();
+			}
+		}
+			
 	}
 }
 
@@ -80,8 +100,7 @@ void listAndSaveResult(BinaryBlock[] blocks)
 		foreach (i, blk; blocks)
 			writefln("%3d. %s", i, blk);
 	}
-	auto bfw = BinaryFileWriter(openOutputFile());
-	bfw.writeFile(blocks);
+	BinaryFileWriter(openOutputFile()).writeFile(blocks);
 }
 
 void merge(string[] args)
@@ -90,6 +109,36 @@ void merge(string[] args)
 	foreach (file; InputFiles(args))
 		result ~= BinaryFileReader(file).readFile();
 	listAndSaveResult(result);
+}
+
+void extract(string[] args)
+{
+	BinaryBlock[] blocks;
+	foreach (file; InputFiles(args))
+	{
+		uint bn = 0;
+		blocks = BinaryFileReader(file).readFile();
+		while (position > 0 && bn < blocks.length)
+		{
+			bn++;
+			position--;
+		}
+		if (bn >= blocks.length)
+			continue;
+		
+		if (verbose && outputFile.length)
+		{
+			writeln(file.name, ":");
+			writefln("%3d. %s", bn, blocks[bn]);
+		}
+		auto of = openOutputFile();
+		if (removeHeader)
+			of.rawWrite(blocks[bn].data);
+		else
+			BinaryFileWriter(of).writeFile(blocks[bn]);
+		return;
+	}
+	throw new Exception("Nothing to extract");
 }
 
 void unpack(string[] args)
@@ -108,6 +157,25 @@ void pack(string[] args)
 	listAndSaveResult(result);
 }
 
+void disassembly(string[] args)
+{
+	auto of = openOutputFile();
+	foreach (file; InputFiles(args))
+	{
+		if (args.length > 3)
+			of.writeln(file.name, ":");
+		foreach (i, blk; BinaryFileReader(file).readFile())
+		{
+			of.writefln("\n; %3d. %s", i, blk);
+			of.writefln("\n\torg $%04x\n", blk.addr);
+			foreach (k; new Disassembler(blk))
+			{
+				of.writefln("\t%s", k);
+			}
+		}
+	}
+}
+
 void printHelp(string[] args)
 {
 	debug {} else write(
@@ -115,23 +183,33 @@ void printHelp(string[] args)
 		"\nUsage:\n" ~
 		args[0] ~ " command [options] [input_file ...]\n" ~
 		"\nThe following commands are available:\n" ~
-		" l[ist]                              list blocks inside file\n" ~
-		" m[erge]                             merge input files into single file\n" ~
-		" p[ack]   [-a=ad] [-s] [-o=fn] [-v]  pack using FlashPack algorithm\n" ~
-		" u[npack] [-o=fn]                    unpack FlashPack'd file\n" ~
-		" h[elp]                              print this message\n" ~
+		" l[ist]                                list blocks inside file\n" ~
+		" m[erge]   [-o=fn] [-v]                merge input files into single file\n" ~
+		" e[xtract] [-n=pos] [-r] [-o=fn] [-v]  extract block\n" ~
+/+ TODO:
+		" r[emove]  [-n=pos] [-o=fn] [-v]       remove block from file\n" ~
+		" i[nsert]  [-n=pos] [-a=ad] [-o=fn] [-v]  insert block into file\n" ~ 
+		" o[ptimize] [-o=fn] [-i]               optimize file\n +/
+		" d[isasm]  [-o=fn]                     disassemble blocks\n" ~
+		" p[ack]    [-a=ad] [-s] [-o=fn] [-v]   pack using FlashPack algorithm\n" ~
+		" u[npack]  [-o=fn] [-v]                unpack FlashPack'd file\n" ~
+		" h[elp]                                print this message\n" ~
 		"\nOptions:\n" ~
 		" -o|--output=fn           set output file name to fn (defaults to stdout)\n" ~
 //		" -a|--address=[-]ad       set packed/boxed data address to ad\n" ~
 		" -a|--address=[-]ad       set packed data address to ad\n" ~
 		"                          (use '-' for end address)\n" ~
+		" -n|--position=pos        which block to extract (indexed from 0)\n" ~
+		" -r|--raw                 remove header from extracted block\n" ~
+//		" -i|--ignore-order        do not preserve order of block if it makes\n" ~
+//		"                          optimized file shorter\n" ~
 //		" -n|--position=pos        set block position for extract, delete, insert\n" ~
 //		"                          (indexed from 0)\n" ~
 		" -s|--disable-os          make depacker running with OS ROM disabled\n" ~
 		" -v|--verbose             emit some junk to stdout (requires output file\n" ~
 		"                          to be specified)\n" ~
-		"\nIf input file is not specified, stdin is used as input\n" ~
-		"\n'=' character between option name and its parameter may be skipped\n"
+		"\nIf input file is not specified, stdin is used as input.\n" ~
+		"\n'=' character between option name and its parameter may be skipped.\n"
 		);
 }
 
@@ -183,7 +261,8 @@ int main(string[] args)
 		config.noBundling,
 		"s|disable-os", &disableOs,
 		"a|address", &strAddr,
-//		"n|position", &position,
+		"n|position", &position,
+		"r|raw", &removeHeader,
 		"v|verbose", &verbose,
 		"o|output", &outputFile);
 
@@ -195,9 +274,11 @@ int main(string[] args)
 		auto funcs = [
 			"help":&printHelp,
 			"list":&list,
+			"extract":&extract,
 			"merge":&merge,
 			"unpack":&unpack,
-			"pack":&pack
+			"pack":&pack,
+			"disasm":&disassembly
 			];
 		foreach (cmd, fun; funcs)
 		{
