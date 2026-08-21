edfe6af968fa919c408a7443a58df66c4e4baf1f
Adrian Matoga <adrian@matoga.info>
2026-08-21 13:17:11 +0200

    Re-export xasm as xebin's module

---
+++ source/xebin/xasm.d
@@ -0,0 +1,20 @@
+/**	Re-exports the xasm 6502 cross-assembler as part of xebin's public API.
+
+	Poetic License:
+
+	This work 'as-is' we provide.
+	No warranty express or implied.
+	We've done our best,
+	to debug and test.
+	Liability for damages denied.
+
+	Permission is granted hereby,
+	to copy, share, and modify.
+	Use as is fit,
+	free or for profit.
+	These rights, on this notice, rely.
+*/
+
+module xebin.xasm;
+
+public import xasm;
