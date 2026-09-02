89953978a140669e1eb6a1eda842468239a31ddf
Adrian Matoga <adrian@matoga.info>
2026-09-02 21:49:42 +0200

    Switch to zlib license

--- source/app.d
+++ source/app.d
@@ -1,23 +1,26 @@
-/*	(Written in D programming language)
-
-	xebin - Atari XL/XE Binary File Utility
+/*	xebin - Atari XL/XE Binary File Utility
 	Command line interface for modules binary and flashpack.
 
-	Author: Adrian Matoga epi@atari8.info
+	Author: Adrian Matoga adrian@matoga.info
 
-	Poetic License:
+	zlib License:
 
-	This work 'as-is' we provide.
-	No warranty express or implied.
-	We've done our best,
-	to debug and test.
-	Liability for damages denied.
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
 
-	Permission is granted hereby,
-	to copy, share, and modify.
-	Use as is fit,
-	free or for profit.
-	These rights, on this notice, rely.
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+	1. The origin of this software must not be misrepresented; you must not
+	   claim that you wrote the original software. If you use this software
+	   in a product, an acknowledgment in the product documentation would be
+	   appreciated but is not required.
+	2. Altered source versions must be plainly marked as such, and must not
+	   be misrepresented as being the original software.
+	3. This notice may not be removed or altered from any source
+	   distribution.
 */
 
 import std.stdio;
